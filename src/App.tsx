f1514d71b30c6d5d336070451778f3ddb2f6b92b
earnity-omchain <earnitytempo@gmail.com>
2026-06-10 13:01:31 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,13 @@
-import { Switch, Route, Router as WouterRouter, useLocation } from "wouter";
-import { useEffect } from "react";
+import { Router as WouterRouter, Route, Switch } from "wouter";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
-import { WalletProvider } from "@/hooks/use-wallet";
+import Home from "@/pages/home";
 import NotFound from "@/pages/not-found";
-
-// FUXEL Club only
-import ClubLanding from "@/pages/fuxel-club/landing";
-import ClubHome from "@/pages/fuxel-club/home";
-import ClubLeaderboard from "@/pages/fuxel-club/leaderboard";
-import ShowHands from "@/pages/fuxel-club/show-hands";
-import HowTo from "@/pages/fuxel-club/how-to";
-import Deck from "@/pages/fuxel-club/deck";
-
-function Redirect({ to }: { to: string }) {
-  const [, navigate] = useLocation();
-  useEffect(() => navigate(to, { replace: true }), [navigate, to]);
-  return null;
-}
 
 function Router() {
   return (
     <Switch>
-      {/* Root → club landing */}
-      <Route path="/" component={() => <Redirect to="/club" />} />
-
-      {/* FUXEL Club */}
-      <Route path="/club" component={ClubLanding} />
-      <Route path="/club/home" component={ClubHome} />
-      <Route path="/club/board" component={ClubLeaderboard} />
-      <Route path="/club/hands" component={ShowHands} />
-      <Route path="/club/how" component={HowTo} />
-      <Route path="/club/deck" component={Deck} />
-
-      {/* 404 */}
+      <Route path="/" component={Home} />
       <Route component={NotFound} />
     </Switch>
   );
@@ -41,14 +15,12 @@ function Router() {
 
 function App() {
   return (
-    <WalletProvider>
-      <TooltipProvider>
-        <WouterRouter>
-          <Router />
-        </WouterRouter>
-        <Toaster />
-      </TooltipProvider>
-    </WalletProvider>
+    <TooltipProvider>
+      <WouterRouter>
+        <Router />
+      </WouterRouter>
+      <Toaster />
+    </TooltipProvider>
   );
 }
 
